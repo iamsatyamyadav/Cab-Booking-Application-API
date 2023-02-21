0a0df949aeb54a241e75512be948a153ffdb2f96
iamsatyamyadav <iamsatyam2002@gmail.com>
2023-02-21 22:59:35 +0530

    Added models

---
+++ CabWalla/src/main/java/com/CabWalla/CabWallaApplication.java
@@ -0,0 +1,13 @@
+package com.CabWalla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabWallaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabWallaApplication.class, args);
+	}
+
+}
---
+++ CabWalla/src/test/java/com/CabWalla/CabWallaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CabWalla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabWallaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
