bc33b5f4d5bcbb6bffc3c47bc8f3bba8faaab824
sudhir2408 <sudhir.43215@gmail.com>
2023-02-22 22:32:30 +0530

    sudhir_day1_task

---
+++ CabWalla/src/main/java/com/CabWalla/model/Customer.java
@@ -0,0 +1,48 @@
+package com.CabWalla.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+   //@Data
+   @Getter
+   @Setter
+   @ToString
+   @AllArgsConstructor
+   @NoArgsConstructor
+   @Entity
+public class Customer extends AbstractUser{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer customerId;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "customer")
+	private Set<TripBooking> tripBookings = new HashSet<>();
+
+	public Customer(String userName, String password, String address, String mobileNo, String email,
+			Integer customerId) {
+		super(userName, password, address, mobileNo, email);
+		this.customerId = customerId;
+	}
+
+	public Customer(String userName, String password, String address, String mobileNo, String email) {
+		super(userName, password, address, mobileNo, email);
+	}
+	
+	
+	
+	
+}
