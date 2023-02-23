b7b396c66dc5290f5ab9ae711d331c7f789c8ebd
Saurabh Patel <saurabh.patel0894@gmail.com>
2023-02-24 00:05:25 +0530

    trip_exception

---
+++ CabWalla/src/main/java/com/CabWalla/exception/TripBookingException.java
@@ -0,0 +1,18 @@
+package com.CabWalla.exception;
+
+public class TripBookingException extends Exception{
+
+	public TripBookingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TripBookingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
