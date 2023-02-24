a32a12a03d6506e84b6956bd02e135f2eabaeaae
sudhir2408 <sudhir.43215@gmail.com>
2023-02-24 14:12:00 +0530

    sudhir-day2-task

---
+++ CabWalla/src/main/java/com/CabWalla/repository/CustomerDao.java
@@ -0,0 +1,17 @@
+package com.CabWalla.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.CabWalla.model.Customer;
+import com.CabWalla.model.CustomerValidationDTO;
+
+@Repository
+public interface CustomerDao extends JpaRepository<Customer, Integer>{
+
+	public Customer findByUserName(String userName);
+	
+	@Query("select new com.cabWalla.model.CustomerValidationDTO(c.userName,c.password) from Customer c")
+	public CustomerValidationDTO findCustomerUserNamePassword(String userName, String password);
+}
